e037a28d080f329b1d43781341ba816e956c66c8
Siddharth Singh <dev_siddharth@icloud.com>
2025-03-01 16:31:51 +0000

    working app v2

--- model_serving/server.py
+++ model_serving/server.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 import joblib
 import numpy as np
-
+import configparser
+import os
 import boto3
-# ABC 
-# AWS S3 Configuration
-AWS_ACCESS_KEY = "AKIASVQKHTXCBUOO4VP2"
-AWS_SECRET_KEY = "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"
-S3_BUCKET_NAME = "siddharthsingh5010mybucket"
+
+# Reading Access and Secret Key
+config = configparser.ConfigParser()
+config.read("app/.awscredentials")
+AWS_ACCESS_KEY = config['default']['aws_access_key_id']
+AWS_SECRET_KEY = config['default']['aws_secret_access_key']
+S3_BUCKET_NAME = config['default']['bucket_name']
 S3_MODEL_PATH = "iris_model/model/best_model.pkl" 
 
 LOCAL_MODEL_PATH = "app/models/best_model.pkl" 
@@ -27,6 +30,22 @@ def read_root():
 
 @app.post("/predict")
 def predict(data:dict):
+    """
+    Predict the class name based on the provided features.
+    Args:
+        data (dict): A dictionary containing the input data. 
+                        The dictionary should have a key 'features' 
+                        which maps to a list or array of feature values.
+    Returns:
+        dict: A dictionary containing the prediction result with the key 'prediction' 
+                mapping to the predicted class name.
+    Example:
+        data = {
+            "features": [0.5, 1.2, 3.4, 2.1]
+        }
+        result = predict(data)
+        # result will be something like {'prediction': 'class_name'}
+    """
     features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)
     class_name = class_names[prediction][0]
